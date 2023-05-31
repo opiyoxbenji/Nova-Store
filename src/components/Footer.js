0e3b869a19790051d807c4fdb9c04467fd3743eb
opiyoxbenji <opiyosbenji@gmail.com>
2023-05-31 16:24:22 +0300

    finished working on and styling the header section of the home page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+	return (
+		<>
+			<footer className='py-3'>
+				<div className='container-xxl'>
+					<div className='row'>
+						<div className='col-5'>
+							<div className='footer-top-data d-flex gap-30 align-items-center'></div>
+						</div>
+					</div>
+				</div>
+			</footer>
+			<footer className='py-3'></footer>
+			<footer className='py-4'>
+				<div className='row'>
+					<div className='col-12'>
+						<div className='text-center mb-0 text-white'>
+							&copy; {new Date().getFullYear()} Powered by Nova
+							Nexus
+						</div>
+					</div>
+				</div>
+			</footer>
+		</>
+	);
+};
 
-export default Footer
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,164 @@
-import React from 'react'
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { BsSearch } from 'react-icons/bs';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+	return (
+		<>
+			<header className='header-top-strip py-3'>
+				<div className='container-xxl'>
+					<div className='row'>
+						<div className='col-6'>
+							<p className='text-white mb-0'>
+								Free Shipping Over $100 & Free Returns
+							</p>
+						</div>
+						<div className='col-6'>
+							<p className='text-end text-white mb-0'>
+								Hotline:
+								<a
+									className='text-white'
+									href='tel:+256 752684025'
+								>
+									+256 752684025
+								</a>
+							</p>
+						</div>
+					</div>
+				</div>
+			</header>
+			<header className='header-upper py-3'>
+				<div className='container-xxl'>
+					<div className='row align-items-center'>
+						<div className='col-2'>
+							<h2>
+								<Link className='text-white'>Nova Store</Link>
+							</h2>
+						</div>
+						<div className='col-5'>
+							<div className='input-group'>
+								<input
+									type='text'
+									className='form-control py-2'
+									placeholder='Search for anything'
+									aria-label='Search'
+									aria-describedby='basic-addon2'
+								/>
+								<span
+									className='input-group-text'
+									id='basic-addon2'
+								>
+									<BsSearch className='fs-6' />
+								</span>
+							</div>
+						</div>
+						<div className='col-5'>
+							<div className='header-upper-links d-flex justify-content-between'>
+								<div>
+									<Link className='d-flex align-items-center gap-10'>
+										<img
+											src='images/compare.svg'
+											alt='compare'
+										/>
+										<p className='mb-0 text-white'>
+											Compare <br /> Products
+										</p>
+									</Link>
+								</div>
+								<div>
+									<Link className='d-flex align-items-center gap-10'>
+										<img
+											src='images/wishlist.svg'
+											alt='wishlist'
+										/>
+										<p className='mb-0 text-white'>
+											Favorite <br /> Items
+										</p>
+									</Link>
+								</div>
+								<div>
+									<Link className='d-flex align-items-center gap-10'>
+										<img src='images/user.svg' alt='user' />
+										<p className='mb-0 text-white'>Login</p>
+									</Link>
+								</div>
+								<div>
+									<Link className='d-flex align-items-center gap-10'>
+										<img src='images/cart.svg' alt='cart' />
+										<div className='d-flex flex-column gap-10'>
+											<span className='badge bg-white text-dark'>
+												0
+											</span>
+											<p className='mb-0 text-white'>
+												$ 0.00
+											</p>
+										</div>
+									</Link>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+			<header className='header-bottom py-3'>
+				<div className='container-xxl'>
+					<div className='row'>
+						<div className='menu-bottom d-flex align-items-center gap-30'>
+							<div>
+								<div class='dropdown'>
+									<button
+										className='btn btn-secondary dropdown-toggle border-0 bg-transparent border-0  gap-15 align-items-center'
+										type='button'
+										data-bs-toggle='dropdown'
+										aria-expanded='false'
+									>
+										<img src='images/menu.svg' alt='menu' />
+										<span className='d-inline-block'>
+											Categories
+										</span>
+									</button>
+									<ul class='dropdown-menu'>
+										<li>
+											<Link
+												className='dropdown-item text-white'
+												to=''
+											>
+												Action
+											</Link>
+										</li>
+										<li>
+											<Link
+												className='dropdown-item text-white'
+												to=''
+											>
+												Another action
+											</Link>
+										</li>
+										<li>
+											<Link
+												className='dropdown-item text-white'
+												to=''
+											>
+												Something else here
+											</Link>
+										</li>
+									</ul>
+								</div>
+							</div>
+							<div className='menu-links'>
+								<div className='d-flex align-items-center gap-15'>
+									<NavLink to='/home'>Home</NavLink>
+									<NavLink to='/store'>Our Store</NavLink>
+									<NavLink to='/blogs'>Blogs</NavLink>
+									<NavLink to='/contact'>Contact</NavLink>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+		</>
+	);
+};
 
-export default Header
+export default Header;
